914e116a1276fa7e5f59f7a6a72aaa1cd99d9bd8
Craig Lager <craigl@aferry.com>
2023-11-21 09:12:33 +0000

    Update profile.ps1

--- profile.ps1
+++ profile.ps1
@@ -1,49 +1,61 @@
+$scriptPath = $MyInvocation.MyCommand.Path
+Write-Host "Loading Profile $scriptPath"
+
+
+Write-Host "ENV Loaded"
 
 Import-Module posh-git
-
-$Env:NPM_TOKEN = 'xxx'
+Write-Host "posh-git Loaded"
 
 New-Alias e explorer
-New-Alias gits git-status
-New-Alias gitc git-commit
-New-Alias gita git-add-all
-New-Alias gitph git-push
-New-Alias gitpl git-pull
-New-Alias gitsync git-sync
-New-Alias go cdls
 
+New-Alias gita git-add-all
 function git-add-all{
 		git add .
 }
 
+New-Alias gitc git-commit
 function git-commit {
 	param ([string] $m)
 	git commit -a -m $m
 }
 
+New-Alias gitph git-push
 function git-push{
 	git push
 }
 
+New-Alias gitpl git-pull
 function git-pull{
 	git pull
 }
 
+New-Alias gitsync git-sync
 function git-sync{
 	git pull
 	git push
 }
 
+New-Alias gits git-status
 function git-status{
 		git status
 }
 
-function cdls{
-	param ([string] $path)
-	cd $path
-	ls
+New-Alias gitacp git-acp
+function git-acp {
+    param ([string] $m)
+    git add .
+    git commit -m $m
+    git push
 }
 
-D:
-cd repos
-ls
+New-Alias go cdls
+function cdls{
+	param ([string] $path)
+	Set-Location $path
+	Get-ChildItem
+}
+
+Write-Host "Functions Set"
+Clear-Host
+Get-ChildItem
